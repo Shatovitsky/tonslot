d985f7811d1e39a73fd32032f4ecfee693d5b2a8
Shatovitsky <92272028+Shatovitsky@users.noreply.github.com>
2024-01-24 23:06:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
+const mongoose = require('mongoose');
 
+const mongoDBURL = 'mongodb+srv://casinohack:41155qweasdzxc@cluster0.afkand9.mongodb.net/bots-clients';
+mongoose.connect(mongoDBURL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Подключено к MongoDB Atlas'))
+    .catch((err) => console.error('Ошибка при подключении к MongoDB Atlas:', err));
 const TOKEN = "2132598511:AAH4ERYL4gVJ3YI4TpsBT6ZL05ubAEH2h0g";
 const game1Name = "Rocket_Queen";
 /* const game2Name = "Lucky_Jet";
@@ -10,6 +15,23 @@ const game3Name = "Aviator";  */
 const game2Name = "jaja_lucky";
 const game3Name = "hack";
 
+// Определение схемы для хранения chat_id
+const userSchema = new mongoose.Schema({
+    chatId: {
+        type: Number,
+        unique: true
+    },
+    username: String,
+    firstName: String,
+    lastName: String,
+    languageCode: String,
+    isBot: Boolean
+});
+
+// Создание модели
+const User = mongoose.model('User', userSchema, 'bot-ru');
+const adminId = 1056931879;
+const bot = new TelegramBot(TOKEN, { polling: true });
 
 const buttonTitle = "Получить прогноз"; 
 const welcomePhoto = "https://play-lh.googleusercontent.com/W-xMaa2x_2fJGBA8qC3rm9anD_6o8IJ9_ttkFrzy7Y8NAZtAyuAuRM62mq_SwAs2D-cY";
@@ -28,6 +50,31 @@ server.use(express.static(path.join(__dirname, "Lucky_Jet")));
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
+    const userId = msg.from.id;
+    const username = msg.from.username;
+    const firstName = msg.from.first_name;
+    const lastName = msg.from.last_name;
+    const languageCode = msg.from.language_code;
+    const isBot = msg.from.is_bot;
+
+    
+    const existingUser = await User.findOne({ chatId: chatId });
+
+    if (!existingUser) {
+    const newUser = new User({
+        chatId: chatId,
+        username: username,
+        firstName: firstName,
+        lastName: lastName,
+        languageCode: languageCode,
+        isBot: isBot
+    });
+    await newUser.save();
+    // Убрано текстовое сообщение
+} else {
+    // Убрано текстовое сообщение
+}
+    
     const welcomeMessage = "Для работоспособности (подключения) данного бота писать ➡️ @casinohack_ivan";
     const replyMarkup = {
         inline_keyboard: [
